fa28bd1ff39ad2c24c8eebe0ba0fee74cf3dcfee
BasmalahGad <101473200+BasmalahGad@users.noreply.github.com>
2023-10-28 22:40:42 +0200

    echo added

--- CLI/src/Terminal.java
+++ CLI/src/Terminal.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class Terminal {
-    private static Parser parser;
+    private String output = "";
+    private static Parser parser = new Parser();
     public void chooseCommandAction() {
 
     }
@@ -13,6 +14,28 @@ public class Terminal {
             if (!input.isEmpty()) {
                 parser.parse(input);
             }
+            Terminal t = new Terminal();
+            t.echo();
+            t.display();
+            break;
         }
+    }
+
+    public void echo(){
+        // to be added:
+        // -e option --> enables the interpretation of backslash escapes
+        // * --> echo * = ls
+        // -n --> ???
+
+        // to be handled:
+        // backslash in input without " "
+        // input with " " --> remove " "
+        for (String arg : parser.getArgs()) {
+            output += arg + " ";
+        }
+    }
+
+    public void display(){
+        System.out.println(output);
     }
 }
