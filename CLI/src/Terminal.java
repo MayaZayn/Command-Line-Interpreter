aecf6e6a391f02f53688eb4f61f8bbf06aa907ab
Maya Zain El-Din <11410120210508@stud.cu.edu.eg>
2023-10-28 12:37:33 +0200

    Add rm method

--- CLI/src/Terminal.java
+++ CLI/src/Terminal.java
@@ -1,18 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class Terminal {
     private static Parser parser;
+    public void rm(File file) throws IOException {
+        Files.deleteIfExists(file.toPath());
+    }
     public void chooseCommandAction() {
 
     }
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        while (true) {
-            String input = s.nextLine();
-            input = input.strip();
-            if (!input.isEmpty()) {
-                parser.parse(input);
-            }
-        }
+    public static void main(String[] args) throws IOException {
+//        Scanner s = new Scanner(System.in);
+//        while (true) {
+//            String input = s.nextLine();
+//            input = input.strip();
+//            if (!input.isEmpty()) {
+//                parser.parse(input);
+//            }
+//        }
+        Terminal t = new Terminal();
+        // rm test
+        t.rm(new File("D:\\dest\\1\\New Microsoft Excel Worksheet.xlsx"));
     }
 }
