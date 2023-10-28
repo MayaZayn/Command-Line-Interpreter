8b90ab121e321056e12db5352598cf10fd343908
Maya Zain El-Din <11410120210508@stud.cu.edu.eg>
2023-10-28 12:30:32 +0200

    Add copyDirectory method

--- CLI/src/Terminal.java
+++ CLI/src/Terminal.java
@@ -1,18 +1,38 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class Terminal {
     private static Parser parser;
+    public void copyDirectory(File source, File destination) throws IOException {
+        if (source.isDirectory()) {
+            if (!destination.exists())
+                destination.mkdir();
+            String[] files = source.list();
+            for (String file : files) {
+                File srcFile = new File(source, file);
+                File destFile = new File(destination, file);
+                copyDirectory(srcFile, destFile);
+            }
+        } else {
+            Files.copy(source.toPath(), destination.toPath());
+        }
+    }
     public void chooseCommandAction() {
 
     }
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        while (true) {
-            String input = s.nextLine();
-            input = input.strip();
-            if (!input.isEmpty()) {
-                parser.parse(input);
-            }
-        }
+    public static void main(String[] args) throws IOException {
+//        Scanner s = new Scanner(System.in);
+//        while (true) {
+//            String input = s.nextLine();
+//            input = input.strip();
+//            if (!input.isEmpty()) {
+//                parser.parse(input);
+//            }
+//        }
+        Terminal t = new Terminal();
+        // cp -r test
+//        t.copyDirectory(new File("D:\\source"), new File("D:\\dest"));
     }
 }
