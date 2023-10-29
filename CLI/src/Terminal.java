6319947f021241511038d2416ba7ccf3aaf26c9a
Rawan Younis <rawannyounis@gmail.com>
2023-10-29 08:18:38 +0200

    Add history function

--- CLI/src/Terminal.java
+++ CLI/src/Terminal.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Terminal {
-    private static Parser parser;
+    private static Parser parser = new Parser();
+    static List<String> inputHistory = new ArrayList<>();
+    public static String history(){
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < inputHistory.size(); i++) {
+            builder.append(i + 1).append("  ").append(inputHistory.get(i)).append("\n");
+        }
+        return builder.toString();
+    }
     public void chooseCommandAction() {
 
     }
@@ -13,6 +23,8 @@ public class Terminal {
             if (!input.isEmpty()) {
                 parser.parse(input);
             }
+//            inputHistory.add(input);
+//            System.out.println(history());
         }
     }
 }
