41b74d3874745b377b583698882586e375711d37
Rawan Younis <rawannyounis@gmail.com>
2023-10-29 08:04:59 +0200

    Add cat function

--- CLI/src/Terminal.java
+++ CLI/src/Terminal.java
@@ -1,7 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Terminal {
-    private static Parser parser;
+    private static Parser parser = new Parser();
+    public static String cat() {
+        //handle paths with empty space
+        //Reset arguments list in parser
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < parser.getArgs().size(); i++) {
+            String filename= parser.getArgs().get(i);
+            File file = new File(filename);
+            try {
+                Scanner fileSc = new Scanner(file);
+                while(fileSc.hasNextLine()){
+                    result.append(fileSc.nextLine()).append("\n");
+                }
+            }catch (FileNotFoundException fileNotFoundException){
+                return "File not Found";
+            }
+        }
+        return result.toString();
+    }
     public void chooseCommandAction() {
 
     }
@@ -13,6 +33,8 @@ public class Terminal {
             if (!input.isEmpty()) {
                 parser.parse(input);
             }
+//            System.out.println(cat());
+//            parser.resetArgs();
         }
     }
 }
