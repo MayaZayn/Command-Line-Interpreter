223aaf860e5613bca2b0220f168d806c526beea7
BasmalahGad <101473200+BasmalahGad@users.noreply.github.com>
2023-11-01 19:53:02 +0200

    cd added

--- CLI/src/Terminal.java
+++ CLI/src/Terminal.java
@@ -1,8 +1,12 @@
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Terminal {
     private String output;
-    private String currentDir;
+    private Path currentDir = Paths.get("").toAbsolutePath();
     private static Parser parser = new Parser();
     public void chooseCommandAction() {
 
@@ -16,25 +20,43 @@ public class Terminal {
                 parser.parse(input);
             }
             Terminal t = new Terminal();
-            t.echo();
+            t.cd();
+            //t.pwd();
+
             t.display();
             break;
         }
     }
 
-    public void cd(){
+    public void cd() {
+        if (parser.getArgs().isEmpty()){ // cd
+            currentDir = currentDir.resolve(currentDir.getRoot());
+        }else{
+            String path = "";
+            for (String arg : parser.getArgs()) {
+                path += arg + " ";
+            }
+            path = path.substring(0, path.length() - 1);
 
-    public void pwd(){
-        // test if error بيضرب
-        // import java.nio.file.Paths;
-        if (parser.getArgs().size() > 0){
-            System.err.println("bad arguments, pwd takes no arguments.");
+            Path newPath;
+            try {
+                newPath = Paths.get(path);
+                if (!newPath.isAbsolute()){
+                    path = currentDir + "\\" + path;
+                    newPath = Paths.get(path);
+                }
+                if(Files.exists(newPath)){
+                    currentDir = currentDir.resolve(newPath);
+                }else {
+                    System.err.println("bad arguments, invalid path given.");
+                }
+            }catch (InvalidPathException ex) {
+                System.err.println("bad arguments, invalid path given.");
+            }
         }
-        currentDir = "";
-        currentDir = Paths.get("").toAbsolutePath().toString();
-        output = currentDir;
-    }
 
+
+    }
 
     public void display(){
         System.out.println(output);
